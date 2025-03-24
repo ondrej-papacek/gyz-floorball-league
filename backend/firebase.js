d5ba88f41591584ca94404ada848ee42d7e5b80b
Ondrej Papacek <ondrejpapacek@gmail.com>
2025-03-24 19:27:28 +0100

    backend/firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,5 +1,7 @@
 ﻿const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
+const raw = process.env.FIREBASE_CONFIG;
+const serviceAccount = JSON.parse(raw.replace(/\\n/g, '\n'));
 
 if (!admin.apps.length) {
    admin.initializeApp({
