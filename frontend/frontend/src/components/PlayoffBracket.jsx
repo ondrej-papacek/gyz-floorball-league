9da29771d8f01a182e40ef54f86b143ac89ab894
Ondřej Papáček <ondrej.papacek@inagami.com>
2025-05-04 09:42:54 +0200

    bracket - styling fix1

--- frontend/frontend/src/components/PlayoffBracket.jsx
+++ frontend/frontend/src/components/PlayoffBracket.jsx
@@ -1,15 +1,22 @@
 ﻿import React from 'react';
 import './playoffBracket.css';
-import { SingleEliminationBracket, Match } from '@g-loot/react-tournament-brackets';
+import {
+    SingleEliminationBracket,
+    Match as DefaultMatch,
+} from '@g-loot/react-tournament-brackets';
+
+const CustomMatch = ({ startTime, ...rest }) => {
+    return <DefaultMatch {...rest} />;
+};
 
 const PlayoffBracket = ({ matches }) => {
     if (!matches?.length) return <p>Žádné zápasy zatím nejsou dostupné.</p>;
 
     return (
-        <div className="playoff-bracket">
+        <div className="playoff-bracket-reset">
             <SingleEliminationBracket
                 matches={matches}
-                matchComponent={Match}
+                matchComponent={CustomMatch}
                 roundSeparatorWidth={64}
                 svgWrapperProps={{ className: 'bracket-svg-wrapper' }}
             />
